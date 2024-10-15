256d23c734dd75455e71bd51efeec46a84e6821c
YogeshH1987 <97949898+YogeshH1987@users.noreply.github.com>
2024-10-15 21:09:57 +0530

    First Commit

---
+++ Gruntfile.js
@@ -0,0 +1,162 @@
+module.exports = function(grunt) {
+  // Project configuration
+  grunt.initConfig({
+    // SCSS compilation task
+    sass: {
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src/scss',
+            src: ['global.scss'],
+            dest: 'assets/css',
+            ext: '.css'
+          },
+          {
+            expand: true,
+            cwd: 'src/scss',
+            src: ['vendor.scss'],
+            dest: 'assets/css',
+            ext: '.css'
+          },
+          {
+            expand: true,
+            cwd: 'src/scss/components',  
+            src: ['*.scss'],            
+            dest: 'src/components',      
+            ext: '.css',
+            rename: function(dest, src) {
+              const componentName = src.split('.')[0];
+              const outputPath = `${dest}/${componentName}/${componentName}.css`;
+              console.log(`Compiling ${src} to ${outputPath}`);
+              return outputPath;
+            }
+          }
+        ]
+      }
+    },
+
+    // Local server configuration
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: ['assets', 'assets/css', 'assets/images', 'src', 'src/pages'],
+          livereload: true,
+          open: true,
+          middleware: function(connect, options, middlewares) {
+            middlewares.unshift(function(req, res, next) {
+              console.log(`Received request for ${req.url}`); // Log every request
+              if (req.url.endsWith('.css')) {
+                res.setHeader('Content-Type', 'text/css');
+                console.log(`Serving ${req.url} with Content-Type: text/css`);
+              }
+              return next();
+            });
+            return middlewares;
+          }
+          
+        }
+      }
+    },    
+
+    // Watch task for SCSS, HTML, and JavaScript files
+    watch: {
+      sass: {
+        files: ['src/scss/**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          livereload: true,
+          spawn: false // Optional: speeds up the watch task
+        }
+      },
+      html: {
+        files: ['src/pages/**/*.html'],
+        options: {
+          livereload: true,
+        }
+      },
+      js: {
+        files: ['src/components/**/*.js'],
+        options: {
+          livereload: true,
+        }
+      }
+    },
+
+    // Copy task to create build directory
+    copy: {
+      build: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src/pages',         // Source directory for pages
+            src: ['**'],
+            dest: 'build/pages/'      // Destination directory for pages
+          },
+          {
+            expand: true,
+            cwd: 'src/components',    // Source directory for components
+            src: ['**'],
+            dest: 'build/components/' // Destination directory for components
+          },
+          {
+            expand: true,
+            cwd: 'assets',            // Source directory for assets
+            src: ['**'],
+            dest: 'build/assets/'     // Destination directory for assets
+          },
+          
+        ]
+      }
+    },    
+
+    // Optimize images
+    imagemin: {
+      images: {
+          options: {
+              optimizationLevel: 7,
+              progressive: true
+          },
+          files: [{
+              expand: true,
+              cwd: 'assets/images/',
+              src: ['**/*.{png,jpg,gif}'],
+              dest: 'assets/images/'
+          }]
+      },
+    },
+
+    uglify: {
+      options: {
+        mangle: false,
+        beautify: false,
+        preserveComments: false
+      },
+      build: {
+          files: {
+              'build/assets/js/vendor.js': [
+                  'assets/js/vendor/jquery-3.4.1.min.js',
+                  'assets/js/vendor/swiper-bundle.min.js'
+              ],
+              'build/assets/js/global.js': [
+                  'src/assets/js/global.js'
+              ]
+          }
+      }
+    },
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Register default tasks
+  grunt.registerTask('default', ['sass', 'connect', 'watch', 'copy', 'uglify']);
+  
+  // Register build task
+  grunt.registerTask('build', ['sass', 'copy']);
+};
---
+++ assets/js/global.js
@@ -0,0 +1,18 @@
+var winWidth = $(window).width();
+var winHeight = $(window).height();
+
+$(document).ready(function() {
+});
+
+
+$(window).resize(function(e) {
+    winWidth = $(window).width();
+    winHeight = $(window).height();
+});
+
+$(window).on('load', function(event) {
+    // $('#loaderWrapper').hide();
+});
+
+$(window).on('scroll', function(event) {
+});
