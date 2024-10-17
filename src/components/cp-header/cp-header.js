e1664fe735c5bf9d9a38ecd6736e298e7d693a00
sdhumal2022 <98183564+sdhumal2022@users.noreply.github.com>
2024-10-17 18:52:19 +0530

    HCRC-00:header desktop completed

---
+++ src/components/cp-header/cp-header.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    stickyHeader();
+    hamburgerMenu();
+
+});
+
+function stickyHeader(){
+    var header = $('.cp-header');
+  var offset = header.offset().top;
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > offset) {
+      header.addClass('js-fixed-header');
+    } else {
+      header.removeClass('js-fixed-header');
+    }
+  });
+}
+
+function hamburgerMenu(){
+    $(".open-menu").click(function () {
+        $(this).toggleClass("active");
+      });
+}
