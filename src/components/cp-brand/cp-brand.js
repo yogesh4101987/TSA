a538624154bc3eac213f2000f270b28d0ce23ac2
sdhumal2022 <98183564+sdhumal2022@users.noreply.github.com>
2024-10-22 00:47:24 +0530

    HSRC-00:brand logocomponent added

---
+++ src/components/cp-brand/cp-brand.js
@@ -0,0 +1,25 @@
+var winWidth = $(window).width();
+
+$(document).ready(function () {
+    brandLogo();
+  });
+
+  $(window).resize(function () {
+    brandLogo();
+  })
+
+function brandLogo() {
+    var swiper = new Swiper(".js-brandSlider", {
+      slidesPerView:"auto",
+      //slidesPerGroup:6,
+      loop: false,
+        autoplay: {
+          delay: 3000,
+        },
+      pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+      },
+      
+    });
+  }
