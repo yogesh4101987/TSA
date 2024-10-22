0b784ded25ffdf938016928db4d11f35721bba7d
IngleSourabh <single@merkleinc.com>
2024-10-22 11:48:05 +0530

    css changes done

--- src/components/cp-ecommerce-banner/cp-ecommerce-banner.js
+++ src/components/cp-ecommerce-banner/cp-ecommerce-banner.js
@@ -1,9 +1,9 @@
 var winWidth = $(window).width();
 
 $(document).ready(function() {    
-    if ($(window).width() < 769) {
+    
         ecomHomeBannerSlider();
-    }
+    
 });
 
 $(window).resize(function() {
