79c8d7b70e5e3d3abca52aa637d0804ac3805e6e
Dhanashree <95774223+DhanashreeMerkleinc@users.noreply.github.com>
2024-10-22 14:40:47 +0530

    added  Shop by category section

---
+++ src/components/cp-eCommerce-home-banner/cp-eCommerce-home-banner.js
@@ -0,0 +1,35 @@
+var winWidth = $(window).width();
+
+$(document).ready(function() {    
+    if ($(window).width() < 768) {
+        bannerFeatureMobile();
+    } 
+    else{
+        destroyIsotope();
+    }
+});
+
+$(window).resize(function() {
+    if ($(window).width() < 768) {
+        bannerFeatureMobile();
+    } else{
+        destroyIsotope();
+    }
+});
+
+function bannerFeatureMobile() {
+    var $grid = $('.feature').isotope({
+        itemSelector: '.feature-item',
+        masonry: {
+            columnWidth: '.grid-sizer',  
+            gutter: 6 
+        }
+    });
+}
+
+function destroyIsotope() {
+    var $grid = $('.feature').data('isotope'); 
+    if ($grid) {
+        $grid.destroy(); // Destroy Isotope instance if present
+    }
+}
