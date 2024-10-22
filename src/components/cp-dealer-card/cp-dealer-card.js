e5bd4c5445ef5a61eba4f7b85182a661efac7b05
sdhumal2022 <98183564+sdhumal2022@users.noreply.github.com>
2024-10-22 15:44:01 +0530

    HCRC-00:top dealercomponent added

---
+++ src/components/cp-dealer-card/cp-dealer-card.js
@@ -0,0 +1,25 @@
+
+var winWidth = $(window).width();
+
+$(document).ready(function () {
+ 
+    dealerSlider();
+  
+});
+
+$(window).resize(function () {
+  if ($(window).width() < 769) {
+    dealerSlider();
+  }
+});
+
+function dealerSlider() {
+  var swiper = new Swiper(".js-dealerSlider", {
+    slidesPerView: "auto",
+    loop: false,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
