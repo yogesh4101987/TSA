db3cc49bfcbc7b15529c8bedc599a6f6ffc6ab5f
sdhumal2022 <98183564+sdhumal2022@users.noreply.github.com>
2024-11-11 18:26:15 +0530

    component changes done

--- src/components/cp-select/cp-select.js
+++ src/components/cp-select/cp-select.js
@@ -12,13 +12,13 @@ $(window).resize(function () {
 
 
 function productSelectList(){
-    $('.select-dropdown__button').on('click', function(){
-        $('.select-dropdown__list').toggleClass('active');
+    $('.select-dropdown-button').on('click', function(){
+        $('.select-dropdown-list').toggleClass('active');
     });
-    $('.select-dropdown__list-item').on('click', function(){
+    $('.select-dropdown-list-item').on('click', function(){
         var itemValue = $(this).data('value');
         console.log(itemValue);
-        $('.select-dropdown__button span').text($(this).text()).parent().attr('data-value', itemValue);
-        $('.select-dropdown__list').toggleClass('active');
+        $('.select-dropdown-button span').text($(this).text()).parent().attr('data-value', itemValue);
+        $('.select-dropdown-list').toggleClass('active');
     });
 }
