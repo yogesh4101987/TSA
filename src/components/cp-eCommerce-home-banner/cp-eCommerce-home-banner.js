6dd4b4239e15ade6ff5d1cfad97c399382650a7c
Dhanashree <95774223+DhanashreeMerkleinc@users.noreply.github.com>
2024-11-23 23:26:19 +0530

    added section in wiron-main

--- src/components/cp-eCommerce-home-banner/cp-eCommerce-home-banner.js
+++ src/components/cp-eCommerce-home-banner/cp-eCommerce-home-banner.js
@@ -1,36 +1,65 @@
-var winWidth = $(window).width();
-var swiper;
+let featureSwiper;
 
-$(document).ready(function() {    
-    if (winWidth <= 991) {
-        bannerFeatureMobile();
-    } 
-    else{
-        destroyIsotope();
-    }
+$(document).ready(function () {
+    handleResponsiveFeatures();
 });
 
-$(window).resize(function() {
-    if (winWidth <= 991) {
-        bannerFeatureMobile();
-    } else{
-        destroyIsotope();
-    }
+$(window).resize(function () {
+    handleResponsiveFeatures();
 });
+
+function handleResponsiveFeatures() {
+    const currentWidth = $(window).width();
+
+    if (currentWidth < 768) {
+        destroySwiper();
+        if (!$('.grid-sizer').length) {
+            $('<div class="grid-sizer"></div>').appendTo('.feature');
+        }
+        bannerFeatureMobile();
+    } else {
+        $('.grid-sizer').remove();
+        destroyIsotope();
+        bannerFeatureDesktop();
+    }
+}
 
 function bannerFeatureMobile() {
-    var $grid = $('.feature').isotope({
+    const grid = document.querySelector('.feature');
+    const iso = new Isotope(grid, {
         itemSelector: '.feature-item',
+        layoutMode: 'masonry',
         masonry: {
-            columnWidth: '.grid-sizer',  
-            gutter: 12
-        }
+            columnWidth: '.grid-sizer',
+            gutter: 12,
+        },
     });
 }
 
 function destroyIsotope() {
-    var $grid = $('.feature').data('isotope'); 
+    const $grid = $('.feature').data('isotope');
     if ($grid) {
-        $grid.destroy(); // Destroy Isotope instance if present
+        $grid.destroy();
+    }
+}
+
+function bannerFeatureDesktop() {
+    if (!featureSwiper) {
+        featureSwiper = new Swiper(".js-feature-banner", {
+            slidesPerView: 6,
+            loop: false,
+            navigation: {
+                nextEl: ".swiper-button-next.feature-next",
+                prevEl: ".swiper-button-prev.feature-prev",
+            },
+        });
+        console.log(featureSwiper)
+    }
+}
+
+function destroySwiper() {
+    if (featureSwiper) {
+        featureSwiper.destroy(true, true);
+        featureSwiper = undefined;
     }
 }
