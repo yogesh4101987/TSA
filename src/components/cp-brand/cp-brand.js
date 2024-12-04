c106449eb12f7c402613978a072fc198daee86ef
sdhumal2022 <98183564+sdhumal2022@users.noreply.github.com>
2024-12-04 13:24:40 +0530

    brand slider added

--- src/components/cp-brand/cp-brand.js
+++ src/components/cp-brand/cp-brand.js
@@ -8,18 +8,40 @@ $(document).ready(function () {
     brandLogo();
   })
 
-function brandLogo() {
-    var swiper = new Swiper(".js-brandSlider", {
-      slidesPerView:"auto",
-      //slidesPerGroup:6,
-      loop: true,
-      autoplay: {
-        delay: 3000,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
+// function brandLogo() {
+//     var swiper = new Swiper(".js-brandSlider", {
+//       slidesPerView:"auto",
+//       //slidesPerGroup:6,
+//       loop: true,
+//       autoplay: {
+//         delay: 3000,
+//       },
+//       pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//       },
       
-    });
-  }
+//     });
+//   }
+
+function brandLogo() {
+
+  $('.js-brand-slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ]
+  });
+  
+}
